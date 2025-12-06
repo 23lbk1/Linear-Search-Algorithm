8c1e026b5c0b22757caaf564725530b95b9e2ff9
23lbk1 <23lbk1@queensu.ca>
2025-12-05 22:25:16 -0500

    Improved the app's code

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 def linear_search(num_list, target):
-  # Convert user inputs
   arr = [] # Empty list to store each number in num_list once separated
-  target = int(target) # Establishes target as an integer
-  steps = []  # Stores each step of the search to be displayed on gradio
   
+  # Convert user inputs
   for x in num_list.split(","):
+    if x == "":
+      continue
     x = x.strip() # Makes sure that x is equated to only a number for the given index
-      arr.append(int(x)) # 
-
+    
+    try:
+      arr.append(int(x)) # Establishes the cleaned up number as an integer and appends it to the list arr
+    except ValueError:
+      return "Error: Please enter a list of integers separated by commas."
+    try:
+      target = int(target) # Establishes target as an integer
+    except ValueError:
+      return "Error: Target value must be an integer."
+  
   # Linear Search
   for index, value in enumerate(arr):
     steps.append("Checking index " index ": value = ", value)
 
-      if value == target:
-        steps.append("\n Target ", target, " FOUND at index ", index)
-        return steps
-
+    if value == target:
+      steps.append("\n Target ", target, " FOUND at index ", index)
+      return "\n".join(steps)
+      
   # If no target is found at the end of the loop:
   steps.append("\n Target ", target, " NOT FOUND in the list.")
-  return steps
-
+  return "\n".join(steps)
